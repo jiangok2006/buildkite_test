845a58f3ed7bcfb7660e84a4992c16ec60991bf1
jiangok2006 <jiangok2006@gmail.com>
2024-04-07 23:25:04 -0700

    add ops.sh

--- scripts/ops.sh
+++ scripts/ops.sh
@@ -1,5 +1,15 @@
+#!/bin/sh
 
-export PROJECT_HOME="$HOME/repo/buildkite_test/terraform/production"
+export PROJECT_HOME="$HOME/repo/buildkite_test"
+
+cleanup() {
+    folders=( network compute )
+    for i in "${folders[@]}" 
+    do
+        rm -rf "$PROJECT_HOME/terraform/production/$i/.terragrunt-cache"
+    done
+
+}
 
 apply_all() {
     cd $PROJECT_HOME
